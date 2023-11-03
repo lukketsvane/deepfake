9e1ca824dbba0191dd9ed549c6fea764ab79e01b
lukketsvane <iverfinne@gmail.com>
2023-11-03 19:36:08 +0100

    yes

--- app/page.tsx
+++ app/page.tsx
@@ -10,33 +10,34 @@ const Page: React.FC = () => {
   return (
     <>
       <Head>
-        <title>Deepfake: Benning Hersvåg - Lytt til Skuespillerlinjens Opplesning</title>
-        <link rel="icon" href="/vercel.ico" />
+        <title>Deepfake: Benning Hergsvåg</title>
+        <link rel="icon" href="/path/to/your/custom-icon.ico" />
         {/* Additional SEO configurations can be added here */}
-        <meta name="description" content="Opplev Benning Hersvågs nyeste diktsamling 'Deepfake & Gay' gjennom stemmene til elever ved skuespillerlinjen. En unik sonisk utforskning av moderne eksistens." />
-        <meta property="og:title" content="Deepfake: Benning Hersvåg" />
-        <meta property="og:description" content="Lytt til elever ved skuespillerlinjen formidle Benning Hersvågs 'Deepfake & Gay', en tankevekkende utforskning av den digitale tidsalder." />
-        <meta property="og:image" content="/images/cover.jpg" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.yourdomain.com" />
       </Head>
 
       <main className="min-h-screen bg-gray-100">
         {!showPoetryPlayer ? (
           <div className="w-full h-screen flex flex-col items-center justify-center p-8 bg-black text-white md:flex-row">
             <div className="flex-1 flex justify-center mb-8 md:mb-0">
-              <div className="relative w-2/5 max-w-xs md:max-w-sm md:w-full">
-                <Image src="/images/cover.jpg" alt="Cover" layout="fill" className="rounded-lg" />
+              <div className="relative" style={{ width: '100%', maxWidth: '200px' }}>
+                <Image
+                  src="/images/cover.jpg"
+                  alt="Cover"
+                  layout="responsive"
+                  width={500}
+                  height={750}
+                  className="rounded-lg"
+                />
               </div>
             </div>
-            <div className="flex-1 text-left md:pr-20 md:pl-8">
+            <div className="flex-1 text-center md:text-left md:pl-8 lg:pr-32">
               <h1 className="text-3xl md:text-4xl font-bold mb-2">Deepfake &amp; Gay</h1>
-              <p className="text-xl mb-4">av Benning Hersvåg</p>
+              <p className="text-xl mb-4">av Benning Hergsvåg</p>
               <p className="mb-4">
-                "Deepfake &amp; Gay" er den nyeste diktsamlingen fra Benning Hersvåg, en fremtredende skikkelse innen moderne norsk poesi. Denne samlingen utforsker dyptgående nåtidens kulturelle dynamikker og teknologiens innvirkning på vår virkelighet.
+                Opplev den nyeste diktsamlingen fra Benning Hergsvåg, en prominent figur i norsk poesi. "Deepfake &amp; Gay" utforsker nåtidens kultur og den digitale æraens ambivalens, lest opp av elever ved skuespillerlinjen.
               </p>
               <p>
-                Oppdag "Deepfake &amp; Gay" gjennom stemmene til elever ved skuespillerlinjen, og la deg omslutte av den soniske dybden i Hersvågs poesi. Bli med på en reise gjennom komplekse følelser og skarpe innsikter som definerer vår tid.
+                Bli med på en reise gjennom en serie tankevekkende dikt som avdekker hvordan teknologi kan fordreie vår forståelse av virkeligheten, og utfordrer leseren til å anerkjenne og verdsette kompleksiteten i vår moderne eksistens.
               </p>
               <button
                 onClick={() => setShowPoetryPlayer(true)}
