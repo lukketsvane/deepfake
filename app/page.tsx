75b3e040cb48b3845ad6d2d61e3a85739c701917
lukketsvane <iverfinne@gmail.com>
2023-11-03 20:13:58 +0100

    yes

--- app/page.tsx
+++ app/page.tsx
@@ -34,13 +34,13 @@ const Page: React.FC = () => {
               <h1 className="text-3xl md:text-4xl font-bold mb-2">Deepfake &amp; Gay: Stemmer fra fremtiden</h1>
               <p className="text-xl mb-4">av Benning Hergsvåg</p>
               <p className="mb-4">
-                Med "Deepfake &amp; Gay" tar Benning Hergsvåg, en toneangivende stemme i norsk poesi, oss med på en reise gjennom den digitale tidsalderens labyrint. Denne grensesprengende samlingen står i skjæringspunktet mellom teknologi og humanitet, og utfordrer oss til å betrakte vår virkelighet gjennom et nytt prisme.
+                Med &quot;Deepfake &amp; Gay&quot; tar Benning Hergsvåg, en toneangivende stemme i norsk poesi, oss med på en reise gjennom den digitale tidsalderens labyrint. Denne grensesprengende samlingen står i skjæringspunktet mellom teknologi og humanitet, og utfordrer oss til å betrakte vår virkelighet gjennom et nytt prisme.
               </p>
               <p className="mb-4">
                 Dykk ned i en verden der stemmene til elever ved skuespillerlinjen gir liv til Hergsvågs ord, og formidler en kraftfull og moderne tolkning av poesi. Hver opplesning er en unik forestilling som fanger essensen av Hergsvågs visjonære verk.
               </p>
               <p className="mb-4">
-                Opplev "Deepfake &amp; Gay" ikke bare som skrevne ord, men som en levende, pustende kunstform. Bli med på en sonisk utforskning av hva det betyr å være menneske i et stadig mer digitalisert samfunn.
+                Opplev &quot;Deepfake &amp; Gay&quot; ikke bare som skrevne ord, men som en levende, pustende kunstform. Bli med på en sonisk utforskning av hva det betyr å være menneske i et stadig mer digitalisert samfunn.
               </p>
               <button
                 onClick={() => setShowPoetryPlayer(true)}
