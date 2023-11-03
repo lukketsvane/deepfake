d06b4d57c1566837ee08d910ec50f8426924234d
lukketsvane <iverfinne@gmail.com>
2023-11-03 19:00:57 +0100

    yes

--- app/PoetryPlayer.tsx
+++ app/PoetryPlayer.tsx
@@ -3,65 +3,80 @@ import React, { useState, useEffect, useRef } from 'react';
 
 const PoetryPlayer: React.FC = () => {
   const [poemIndex, setPoemIndex] = useState(0);
-  const poemTitles = ['Deepfake', 'Virtuelle Refleksjoner'];
-  const poems = ['/poems/poem1.txt', '/poems/poem2.txt'];
+  const [introPlayed, setIntroPlayed] = useState(false);
+  const poemTitles = ['Deepfake', 'Virtuelle Refleksjoner', 'Fragmenter', 'Usikker', 'Spiderman'];
+  const poems = ['/poems/poem1.txt', '/poems/poem2.txt', '/poems/poem3.txt', '/poems/poem4.txt', '/poems/poem5.txt'];
   const [poemContent, setPoemContent] = useState('');
-  const [isPlaying, setIsPlaying] = useState(true);
-  const [showOverlay, setShowOverlay] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef<HTMLAudioElement>(null);
+  const introAudioRef = useRef<HTMLAudioElement>(null);
 
+  // Function to fetch poem content
   const fetchPoemContent = async (path: string) => {
     const response = await fetch(path);
     const text = await response.text();
     setPoemContent(text);
   };
 
+  // Function to play the next poem
   const playNextPoem = () => {
     const nextIndex = (poemIndex + 1) % poems.length;
     setPoemIndex(nextIndex);
+    setIsPlaying(true);
+    if (nextIndex === 0) {
+      setIntroPlayed(false);
+    }
   };
 
+  // Effect to fetch poem content when poemIndex changes
   useEffect(() => {
     fetchPoemContent(poems[poemIndex]);
-  // Include poems in the dependency array
-  }, [poemIndex, poems]);
+  }, [poemIndex]);
 
+  // Effect to handle playing and pausing the audio
   useEffect(() => {
-    if (audioRef.current) {
-      if (isPlaying) {
-        audioRef.current.play();
-      } else {
-        audioRef.current.pause();
-      }
+    if (isPlaying && introPlayed) {
+      audioRef.current?.play();
+    } else {
+      audioRef.current?.pause();
     }
-  }, [poemContent, isPlaying]);
+  }, [isPlaying, introPlayed]);
 
+  // Effect to play intro when necessary
+  useEffect(() => {
+    if (poemIndex === 0 && !introPlayed) {
+      setIsPlaying(false);
+      introAudioRef.current?.play();
+    }
+  }, [poemIndex, introPlayed]);
+
+  // Function to toggle play and pause
   const togglePlayPause = () => {
     setIsPlaying(!isPlaying);
   };
 
-  const toggleOverlay = () => {
-    setShowOverlay(!showOverlay);
+  // Function to handle the end of the intro
+  const onIntroEnd = () => {
+    setIntroPlayed(true);
+    setIsPlaying(true);
+  };
+
+  // Function to set the poem index and manage audio state
+  const selectPoem = (index: number) => {
+    setPoemIndex(index);
+    setIsPlaying(false);
+    setIntroPlayed(index === 0 ? false : true);
   };
 
   return (
     <div className="flex w-full h-screen bg-black text-white p-4">
-      <button 
-        onClick={toggleOverlay} 
-        className="md:hidden fixed top-4 right-4 z-20 text-3xl"
-      >
-        {showOverlay ? '✕' : '☰'}
-      </button>
-      <div className={`absolute top-0 left-0 w-full h-full bg-black bg-opacity-80 p-4 transition-transform transform ${showOverlay ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 md:w-1/6 md:fixed md:top-0 md:left-0 md:h-full md:bg-opacity-80`}>
+      <div className={`absolute top-0 left-0 w-full h-full bg-black bg-opacity-80 p-4 transition-transform transform ${introPlayed ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 md:w-1/6 md:fixed md:top-0 md:left-0 md:h-full md:bg-opacity-80`}>
         <ul className="text-white">
           {poemTitles.map((title, index) => (
             <li
               key={title}
               className={`cursor-pointer p-2 ${poemIndex === index ? 'bg-white text-black' : ''}`}
-              onClick={() => {
-                setPoemIndex(index);
-                setShowOverlay(false);
-              }}
+              onClick={() => selectPoem(index)}
             >
               {title}
             </li>
@@ -80,7 +95,7 @@ const PoetryPlayer: React.FC = () => {
             onClick={togglePlayPause}
             className="mt-4 px-4 py-2 bg-black text-white border border-white rounded hover:bg-white hover:text-black focus:outline-none"
           >
-            {isPlaying ? '⏸' : '▶️'}
+            {isPlaying ? 'Pause' : 'Play'}
           </button>
           <button
             onClick={playNextPoem}
@@ -90,9 +105,13 @@ const PoetryPlayer: React.FC = () => {
           </button>
         </div>
         <audio
+          ref={introAudioRef}
+          onEnded={onIntroEnd}
+          src="/audio/intro.mp3"
+        ></audio>
+        <audio
           ref={audioRef}
           onEnded={playNextPoem}
-          autoPlay
           src={`/audio/poem${poemIndex + 1}.mp3`}
         ></audio>
       </div>
